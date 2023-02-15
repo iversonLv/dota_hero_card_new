929b2500215bd475552a254417962c019bf4d5d7
iversonLv <iverson_3_3_3@126.com>
2023-02-15 15:56:22 +0000

    Update 29 files

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 // here we will store STRATZ token, api url, and hero url
 
-export const STRATZ_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJodHRwczovL3N0ZWFtY29tbXVuaXR5LmNvbS9vcGVuaWQvaWQvNzY1NjExOTgwODkwMDc0MDUiLCJ1bmlxdWVfbmFtZSI6Ik1tU29OLkx2IiwiU3ViamVjdCI6ImZhNzEzY2Y0LWZkZWEtNDhlOS1iN2IyLTYxYTNjN2FmZDE4YyIsIlN0ZWFtSWQiOiIxMjg3NDE2NzciLCJuYmYiOjE2NTk5MzgwNDEsImV4cCI6MTY5MTQ3NDA0MSwiaWF0IjoxNjU5OTM4MDQxLCJpc3MiOiJodHRwczovL2FwaS5zdHJhdHouY29tIn0.XsAnUKLVjJ9tISRr7O8ccReBQh2jN2h5hG0dmS7OfYA";
+export const STRATZ_TOKEN = process.env.STRATZ_TOKEN;
 export const STRATZ_API = "https://api.stratz.com/graphql";
 export const STRATZ_HERO_URL = "https://cdn.stratz.com/images/dota2/heroes";
 export const STRATZ_ABILITIE_URL =
